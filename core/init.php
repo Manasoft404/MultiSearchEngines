a4722735e21789f7077764bdacf66a225b7a7588
Manasoft404 <ngudiamanasse@gmail.com>
2018-06-22 08:12:05 -0700

    add function make-url

---
+++ core/init.php
@@ -0,0 +1,4 @@
+<?php
+$path = dirname(_FILE_);
+include("{$path}/inc/functions.inc.php");
+ ?>
--- functions.inc.php
+++ functions.inc.php
@@ -8,7 +8,29 @@ public function make_url($site,$search)
   }else{
     $search = str_replace("","+",htmlentities($search));
   }
-  $search = htmlentities($search);
+  switch (variable) {
+    case 'google':
+      $url ="https://www.google.com/search?q={$search}";
+      break;
+    case 'google_image':
+      $url ="https://www.google.com/images?q={$search}";
+      break;
+    case 'bing':
+      $url ="https://www.bing.com/search?q={$search}";
+      break;
+    case 'bing_image':
+      $url ="https://www.bing.com/images?q={$search}";
+      break;
+    case 'yahoo':
+      $url ="https://ca.search.yahoo.com/web?q={$search}";
+      break;
+    case 'yahoo_image':
+      $url ="https://ca.images.search.yahoo.com/images?q={$search}";
+      break;
+    default:
+      return false
+      break;
+  }
 }
 
 public function display_url($url,$type)
